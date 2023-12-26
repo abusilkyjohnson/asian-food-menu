b69351bba288625463bdb05e7b391e8f530df9a8
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-26 14:46:29 -0500

    adapter class statered with MyViewHolder(i named it that) created list initialized with inflate method

--- app/src/main/java/com/example/chinesefood/AbuAdapter.java
+++ app/src/main/java/com/example/chinesefood/AbuAdapter.java
@@ -1,6 +1,7 @@
 package com.example.chinesefood;
 
 import android.content.ClipData;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,11 +15,26 @@ public class AbuAdapter extends RecyclerView.Adapter<AbuAdapter.MyViewHolder> {
     //2nd
     private List<IndividualDish> individualDishList;
 
+    public AbuAdapter(List<IndividualDish> individualDishList) {
+        this.individualDishList = individualDishList;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //this is responsible for creating new view holder for your items
+        //You inflate the item layout XML and return an instance of your custom Viewholder class.
+
+        //Inflator The layout inflator class is used to inflate or create a view hierarchy from an XML layout
+        //resource.It's an essential part of building UI components in Android, allowing you to convert XML layout definitions into
+        //actual view objects to displayed
+        View dishView = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_layout,parent,false);
+
+
+
+        return new MyViewHolder(dishView);
     }
+    //2nd
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
